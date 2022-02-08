9ef3851f461f1ca84a2a2a4c14c5dd25720ddde6
John Desrosiers <john.desro@gmail.com>
2022-02-08 06:26:24 -0800

    Add cookie expiration

--- config/session.js
+++ config/session.js
@@ -2,6 +2,7 @@ const session = require("express-session");
 const sequelize = require("./connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+// Check for SECRET env var and throw an error if it isn't set.
 if (!process.env.SECRET) {
   // add SECRET=<your session secret> to .env file in local development. Add
   // SECRET to the Heroku config for production.
@@ -11,7 +12,10 @@ if (!process.env.SECRET) {
 // Configure session options
 const sess = {
   secret: process.env.SECRET,
-  cookie: {},
+  cookie: {
+    // cookies expire after 1 day (time in milliseconds)
+    maxAge: 8.64e7
+  },
   resave: false,
   // Wait to save session until the user logs in
   saveUninitialized: false,
